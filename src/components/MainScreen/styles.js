2fc840b8df4156474723df092c146742a157094e
huong.tran <huong.tran@sutrixsolutions.com>
2018-06-19 10:33:19 +0700

    first commit

---
+++ src/components/MainScreen/styles.js
@@ -0,0 +1,56 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  header_location: {
+    justifyContent: "space-between",
+    padding: 20,
+    flexDirection: "row",
+    backgroundColor: "yellow"
+  },
+  products: {
+    flex: 1, 
+    flexDirection: 'row', 
+    padding: 15, 
+    borderBottomWidth: 1,
+    borderBottomColor: 'grey',
+  },
+  product_img: {
+    width: 80, 
+    height: 80
+  },
+  product_infor: {
+    paddingLeft: 15,
+  },
+  product_title: {
+    fontWeight: "bold",
+    fontSize: 18
+  },
+  product_transport: {
+    paddingTop: 8,
+    paddingLeft: 0,
+    flex: 1,
+    flexDirection: "row",
+  },
+  item_transpot: {
+    borderRightWidth: 1,
+    paddingRight: 15,
+    paddingLeft: 0,
+    flexDirection: "row",
+  },
+  item_transpot_1: {
+    borderRightWidth: 1,
+    paddingRight: 15,
+    paddingLeft: 15,
+    flexDirection: "row",
+  },
+  icon_small: {
+    width: 20,
+    height: 20,
+  },
+  text_italic: {
+    fontStyle: 'italic',
+    paddingLeft: 5,
+  }
+  })
+
+export default styles;
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { View, Text } from 'react-native';
 
 import CustomButton from 'components/CustomButton';
+import MainScreen from 'components/MainScreen';
 import styles from './styles';
 
 export default class HomeScreen extends React.Component {
@@ -13,12 +14,7 @@ export default class HomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Home Screen</Text>
-        <CustomButton
-          onPress={() => this.props.navigation.navigate('details')}
-        >
-          <Text>Go to Details</Text>
-        </CustomButton>
+        <MainScreen/>
       </View>
     );
   }
