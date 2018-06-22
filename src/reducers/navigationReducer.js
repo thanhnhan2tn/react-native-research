f2b97fe5abec136f3625b6624983feb6d07066d9
Hoang Tuan OU <hoanganhtuan.ou@gmail.com>
2018-06-22 14:54:04 +0700

    - remove useless code.

--- src/reducers/navigationReducer.js
+++ src/reducers/navigationReducer.js
@@ -16,12 +16,6 @@ function navigationReducer(state = initialNavState, action) {
         state,
       );
       break;
-    case 'listRes':
-      nextState = RootNavigator.router.getStateForAction(
-        NavigationActions.navigate({ routeName: 'listRes' }),
-        state,
-      );
-      break;
     default:
       nextState = RootNavigator.router.getStateForAction(action, state);
       break;
