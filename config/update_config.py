ab92aeb1a328c713082148b6a88bc0ca30825d25
MacTest <MacTest@js-tuanpt.sutrixmedia1.com>
2018-08-02 10:36:11 +0700

    update config version ios

--- config/update_config.py
+++ config/update_config.py
@@ -1,12 +1,18 @@
 import os
 import json
+import plistlib
 
 env = os.environ
+versionStr = env['VERSION_NO'] + '.' + env['BUILD_NO']
 
-print(env)
+pl = plistlib.readPlist("../ios/RNResearch/Info.plist")
+pl['CFBundleShortVersionString'] = versionStr
+pl['CFBundleVersion'] = versionStr
+plistlib.writePlist(pl, "../ios/RNResearch/Info.plist")
+
 with open("package.json", "r") as jsonFile:
     data = json.load(jsonFile)
-    data['version'] = env['VERSION_NO'] + '.' + env['BUILD_NO']
+    data['version'] = versionStr
 
 with open("package.json", "w") as jsonFile:
-    json.dump(data, jsonFile, indent=2, separators=(',', ': '))
+   json.dump(data, jsonFile, indent=2, separators=(',', ': '))
