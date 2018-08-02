c4b7a3faa4f5c1b6baf86d817ec41f22ddb938f4
Hoang Tuan OU <hoanganhtuan.ou@gmail.com>
2018-08-02 10:58:29 +0700

    fix issue wrong path file plist

--- config/update_config.py
+++ config/update_config.py
@@ -5,10 +5,10 @@ import plistlib
 env = os.environ
 versionStr = env['VERSION_NO'] + '.' + env['BUILD_NO']
 
-pl = plistlib.readPlist("../ios/RNResearch/Info.plist")
+pl = plistlib.readPlist("ios/RNResearch/Info.plist")
 pl['CFBundleShortVersionString'] = versionStr
 pl['CFBundleVersion'] = versionStr
-plistlib.writePlist(pl, "../ios/RNResearch/Info.plist")
+plistlib.writePlist(pl, "ios/RNResearch/Info.plist")
 
 with open("package.json", "r") as jsonFile:
     data = json.load(jsonFile)
